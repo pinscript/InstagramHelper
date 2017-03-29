42e473fa0b6edda4789d257b694f607d7f9a145f
Oleg <okoles>
2017-03-29 23:10:57 +0300

    WIP Commit

--- src/background.js
+++ src/background.js
@@ -1,97 +1,65 @@
 /* globals chrome */
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	if (request.action == "show") {
-		chrome.tabs.query({
-			active: true,
-			currentWindow: true
-		}, function (tabs) {
-			chrome.pageAction.show(tabs[0].id);
-		});
-	} else if ("return_insta_users_old" === request.action) {
+(function () {
+	"use strict";
 
-		var url = chrome.extension.getURL('result.html');
-		var reqText = request.text;
+	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		if (request.action == "show") {
+			chrome.tabs.query({
+				active: true,
+				currentWindow: true
+			}, function (tabs) {
+				chrome.pageAction.show(tabs[0].id);
+			});
+		} else if ("return_insta_users" === request.action) {
+			var url = chrome.extension.getURL('instaUsers.html');
+			request.action = "modifyResultPage";
 
-		//query if we already have result page opened
-		chrome.tabs.query({
-			url: url
-		}, function (tabs) {
-			if (0 === tabs.length) { //tab is not found, let's create it
-				chrome.tabs.create({
-					'url': url,
-					'selected': true
-				}, function (tab) {
-					function sendModifyResultPage(tabId, changeInfo, tab) {
-						if (tab.url.indexOf('result.html') != -1 && changeInfo.status == 'complete') {
-							chrome.tabs.sendMessage(tab.id, {
-								"action": "modifyResultPage",
-								"text": reqText
-							});
-							//remove listener
-							chrome.tabs.onUpdated.removeListener(sendModifyResultPage);
+			//query if we already have result page opened
+			chrome.tabs.query({
+				url: url
+			}, function (tabs) {
+				if (0 === tabs.length) { //tab is not found, let's create it
+					chrome.tabs.create({
+						'url': url,
+						'selected': true
+					}, function (tab) {
+						function sendModifyResultPage(tabId, changeInfo, tab) {
+							if (tab.url.indexOf('instaUsers.html') != -1 && changeInfo.status == 'complete') {
+								chrome.tabs.sendMessage(tab.id, request);
+								chrome.tabs.onUpdated.removeListener(sendModifyResultPage);
+							}
 						}
-					}
-					chrome.tabs.onUpdated.addListener(sendModifyResultPage);
-				});
+						chrome.tabs.onUpdated.addListener(sendModifyResultPage);
+					});
 
-			} else { //tab is found, let's show it
-				chrome.tabs.update(tabs[0].id, {
-					active: true
-				});
-				chrome.tabs.sendMessage(tabs[0].id, {
-					"action": "modifyResultPage",
-					"text": reqText
-				});
-			}
-		});
-	} else if ("return_insta_users" === request.action) {
-		var url = chrome.extension.getURL('instaUsers.html');
-		request.action = "modifyResultPage";
-
-		//query if we already have result page opened
-		chrome.tabs.query({
-			url: url
-		}, function (tabs) {
-			if (0 === tabs.length) { //tab is not found, let's create it
-				chrome.tabs.create({
-					'url': url,
-					'selected': true
-				}, function (tab) {
-					function sendModifyResultPage(tabId, changeInfo, tab) {
-						if (tab.url.indexOf('instaUsers.html') != -1 && changeInfo.status == 'complete') {
-							chrome.tabs.sendMessage(tab.id, request);
-							chrome.tabs.onUpdated.removeListener(sendModifyResultPage);
-						}
-					}
-					chrome.tabs.onUpdated.addListener(sendModifyResultPage);
-				});
-
-			} else { //tab is found, let's show it
-				chrome.tabs.update(tabs[0].id, {
-					active: true
-				});
-				chrome.tabs.sendMessage(tabs[0].id, request);
-			}
-		});
-	}
-});
-
-chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
-
-	headers = details.requestHeaders;
-	//modify Referer to make instagram happier
-	for (var header in headers) {
-		if (headers[header].name === "eferer") {
-			headers[header].name = "Referer";
-			break;
+				} else { //tab is found, let's show it
+					chrome.tabs.update(tabs[0].id, {
+						active: true
+					});
+					chrome.tabs.sendMessage(tabs[0].id, request);
+				}
+			});
 		}
-	}
-	return {
-		requestHeaders: details.requestHeaders
-	};
+	});
 
-}, {
-	urls: ["https://www.instagram.com/query/"]
-},
-	['blocking', "requestHeaders"]);
+	chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
+
+		headers = details.requestHeaders;
+		//modify Referer to make instagram happier
+		for (var header in headers) {
+			if (headers[header].name === "eferer") {
+				headers[header].name = "Referer";
+				break;
+			}
+		}
+		return {
+			requestHeaders: details.requestHeaders
+		};
+
+	}, {
+		urls: ["https://www.instagram.com/query/"]
+	},
+		['blocking', "requestHeaders"]);
+
+})();
--- src/instagramHelper.js
+++ src/instagramHelper.js
@@ -1,25 +1,26 @@
 /* jshint esnext: true */
 /* globals chrome, document */
 
-function getCsrfToken() {
-	var id = "InstaHelperInjection";
-	var script = `(function (){
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	"use strict";
+
+	var getCsrfToken = function () {
+		var id = "InstaHelperInjection";
+		var script = `(function (){
 			var ret_value = ((function (){
 				return _sharedData;
 			})());
 			document.getElementById('InstaHelperInjection').innerText = JSON.stringify(ret_value);
 			})();`;
-	var injScript = document.createElement("script");
-	injScript.type = "text/javascript";
-	injScript.innerHTML = script;
-	injScript.id = id;
-	document.head.appendChild(injScript);
-	var ret_value = JSON.parse(injScript.innerText);
-	injScript.parentNode.removeChild(injScript);
-	return ret_value;
-}
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+		var injScript = document.createElement("script");
+		injScript.type = "text/javascript";
+		injScript.innerHTML = script;
+		injScript.id = id;
+		document.head.appendChild(injScript);
+		var ret_value = JSON.parse(injScript.innerText);
+		injScript.parentNode.removeChild(injScript);
+		return ret_value;
+	};
 
 	if (request.action === "get_insta_users") {
 
@@ -38,6 +39,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 		});
 	}
 });
+
 
 chrome.runtime.sendMessage({
 	action: "show"
