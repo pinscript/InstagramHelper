53df81121a567a0955dd034a206d4a0f240454bc
OllegK <OllegK>
2017-06-20 22:54:11 +0300

    Code refactoring preparing for adding "light" mode

--- src/js/FetchUsers.js
+++ src/js/FetchUsers.js
@@ -1,8 +1,119 @@
-var FetchUsers = function(obj){
+var FetchUsers = function (obj, myData, htmlElements, updateStatusDiv, resolve) {
 
+    "use strict";
 
-    
+    var urlTemplate;
+
+    var fetchInstaUsers = function () { //do I need obj?
+        console.log("fetchInstaUsers", this);
+        urlTemplate = `https://www.instagram.com/graphql/query/?query_id=${instaDefOptions.queryId[obj.relType]}&id=${obj.userId}&first=${obj.pageSize}`;
+        obj.url = obj.url || urlTemplate;
+        $.ajax({
+            url: obj.url,
+            context: this,
+            method: 'GET',
+            headers: {
+                "X-CSRFToken": obj.csrfToken,
+                "eferer": "https://www.instagram.com/" + obj.userName + "/"
+            },
+            success: successFetch,
+            error: errorFetch
+        });
+    }
+
+    var successFetch = function (res, textStatus, xhr) {
+        console.log("successFetch", this);
+        obj.receivedResponses += 1;
+        var data = res.data.user[Object.keys(res.data.user)[0]];
+        updateStatusDiv(`received users - ${data.edges.length} (${obj.relType}/${obj.receivedResponses})`);
+        for (let i = 0; i < data.edges.length; i++) {
+            var found = false;
+            if (obj.checkDuplicates) { //only when the second run happens (or we started with already opened result page)
+                for (let j = 0; j < myData.length; j++) {
+                    if (data.edges[i].node.username === myData[j].username) {
+                        found = true;
+                        myData[j]["user_" + obj.relType] = true;
+                        break;
+                    }
+                }
+            }
+            if (!(found)) {
+                data.edges[i].node.user_follows = false; //explicitly set the value for correct search
+                data.edges[i].node.user_followed_by = false; //explicitly set the value for correct search
+                data.edges[i].node["user_" + obj.relType] = true;
+                myData.push(data.edges[i].node);
+            }
+        }
+        updateProgressBar(obj, data.edges.length);
+
+        if (data.page_info.has_next_page) { //need to continue
+            obj.url = `${urlTemplate}&after=${data.page_info.end_cursor}`;
+            setTimeout( () => fetchInstaUsers(), calculateTimeOut(obj));
+            return;
+        }
+        htmlElements[obj.relType].asProgress("finish").asProgress("stop"); //stopProgressBar(obj);
+        if (obj.callBoth) {
+            obj.url = null;
+            obj.relType = obj.relType === "follows" ? "followed_by" : "follows";
+            obj.callBoth = false;
+            obj.checkDuplicates = true;
+            setTimeout( () => fetchInstaUsers(), calculateTimeOut(obj));
+            return;
+        }
+        resolve();
+    }
+
+    var errorFetch = function (jqXHR, exception) {
+        console.log("error ajax");
+        console.log(arguments);
+        if (jqXHR.status === 0) {
+            setTimeout( () => fetchInstaUsers(), instaDefOptions.retryInterval);
+            alert(messages.getMessage("NOTCONNECTED", +instaDefOptions.retryInterval / 60000));
+        } else if (jqXHR.status === 429) {
+            console.log("HTTP429 error.", new Date());
+            retryError(jqXHR.status);
+        } else if (jqXHR.status == 404) {
+            alert(messages.getMessage("HTTP404"));
+        } else if (jqXHR.status == 500) {
+            alert(messages.getMessage("HTTP500"));
+        } else if (exception === 'parsererror') {
+            alert(messages.getMessage("JSONPARSEERROR"));
+        } else if (exception === 'timeout') {
+            alert(messages.getMessage("TIMEOUT"));
+        } else if (exception === 'abort') {
+            alert(messages.getMessage("AJAXABORT"));
+        } else {
+            alert(messages.getMessage("UNCAUGHT", jqXHR.responseText));
+        }
+    }
+
+    function retryError(code) {
+        console.log("HTTP error", new Date());
+        updateStatusDiv(messages.getMessage("HTTP429", +instaDefOptions.retryInterval / 60000), "red");
+        timeout.setTimeout(3000)
+            .then(function () {
+                return countdown.doCountdown("status", "", (new Date()).getTime() + +instaDefOptions.retryInterval)
+            })
+            .then(function () {
+                console.log("Continue execution after HTTP error", new Date());
+                fetchInstaUsers();
+            });
+    }
+
+    function calculateTimeOut(obj) {
+        if (instaDefOptions.noDelayForInit && (obj.receivedResponses < instaDefOptions.requestsToSkipDelay)) {
+            return 0;
+        }
+        return obj.delay;
+    }
+
+    function updateProgressBar(obj, count) {
+        var newValue = 0 + obj[obj.relType + "_processed"] + count;
+        htmlElements[obj.relType].asProgress("go", newValue);
+        obj[obj.relType + "_processed"] = newValue;
+    }
+
     return {
-        
+        fetchInstaUsers: fetchInstaUsers
     }
 };
--- src/js/instaUsers.js
+++ src/js/instaUsers.js
@@ -50,10 +50,43 @@ $(function () {
 			followed_by_processed: 0,
 			startTime: new Date(),
 			timerInterval: startTimer(document.querySelector('#timer'), new Date()),
-			receivedResponses: 0
+			receivedResponses: 0,	//received HTTP responses
+			processedUsers: 0 	//processed users in get full info
 		};
 		prepareHtmlElements(fetchSettings);
-		fetchInstaUsers(fetchSettings);
+		promiseFetchInstaUsers(fetchSettings).then(function(){
+			prepareHtmlElementsUserDetails(fetchSettings, myData);
+			promiseGetFullInfo(fetchSettings, myData).then(function () {
+				generationCompleted(fetchSettings);
+			});
+		});
+	}
+
+	function getFullInfo(obj, arr, resolve) {
+		userInfo.getUserProfile(arr[obj.processedUsers].username).then(function (user) {
+			myData[obj.processedUsers] = $.extend({}, myData[obj.processedUsers], user);;
+			obj.receivedResponses++;
+			htmlElements.intersection.asProgress("go", obj.processedUsers++);
+			if (obj.processedUsers === arr.length) {
+				resolve();
+				return;
+			}
+			setTimeout(function () {
+				getFullInfo(obj, arr, resolve);
+			}, 0);
+		});
+	}
+
+	var updateStatusDiv = function (message, color) {
+		htmlElements.statusDiv.textContent = message;
+		htmlElements.statusDiv.style.color = color || "black";
+	}
+
+	function promiseFetchInstaUsers(obj) {
+		return new Promise(function (resolve, reject) {
+			var f = new FetchUsers(obj, myData, htmlElements, updateStatusDiv, resolve);
+			f.fetchInstaUsers(); 
+		});
 	}
 
 	function startTimer(timer, startTime) {
@@ -70,9 +103,96 @@ $(function () {
 		}, 1000);
 	}
 
-	function updateStatusDiv(message, color) {
-		htmlElements.statusDiv.textContent = message;
-		htmlElements.statusDiv.style.color = color || "black";
+	function showExportDiv(obj) {
+
+		$("#exportDiv").show();
+		$("#export_XLSX").on("click", function () {
+			$("#jqGrid").jqGrid("exportToExcel", {
+				includeLabels: true,
+				includeGroupHeader: false,
+				includeFooter: false,
+				fileName: `user_${obj.userName}_${exportUtils.formatDate(new Date())}.xlsx`,
+				replaceStr: exportUtils.replaceStr
+			});
+		});
+	}
+
+	function prepareHtmlElements(obj) {
+
+		if (obj.callBoth || ("followed_by" === obj.relType)) {
+			document.getElementById("followed_by_title").textContent = `${obj.userName} is followed by ${obj.followed_by_count} users`;
+			document.getElementById("followed_by_title").style.display = "block";
+			htmlElements.followed_by.show().asProgress({
+				namespace: 'progress',
+				min: 0,
+				max: obj.followed_by_count,
+				goal: obj.followed_by_count,
+				labelCallback(n) {
+					const percentage = this.getPercentage(n);
+					return `Followed by:${obj.followed_by_processed}/${obj.followed_by_count}/${percentage}%`;
+				}
+			});
+		}
+		if (obj.callBoth || ("follows" === obj.relType)) {
+			document.getElementById("follows_title").textContent = `${obj.userName} follows ${obj.follows_count} users`;
+			document.getElementById("follows_title").style.display = "block";
+			htmlElements.follows.show().asProgress({
+				namespace: 'progress',
+				min: 0,
+				max: obj.follows_count,
+				goal: obj.follows_count,
+				labelCallback(n) {
+					const percentage = this.getPercentage(n);
+					return `Follows:${obj.follows_processed}/${obj.follows_count}/${percentage}%`;
+				}
+			});
+		}
+	}
+
+	function generationCompleted(obj) {
+		clearInterval(obj.timerInterval);
+		var timer = document.querySelector('#timer');
+		htmlElements.intersection.asProgress("finish").asProgress("stop");
+
+		var diffFollowed = "", diffFollows = "";
+		if (obj.followed_by_count != obj.followed_by_processed) {
+			diffFollowed = `(actually returned ${obj.followed_by_processed})`;
+		}
+		if (obj.follows_count != obj.follows_processed) {
+			diffFollows = `(actually returned ${obj.follows_processed})`;
+		}
+
+		updateStatusDiv(`Completed, spent time - ${timer.textContent}, 
+			created list length - ${myData.length} (follows - ${obj.follows_count}${diffFollows}, 
+			followed by - ${obj.followed_by_count}${diffFollowed}),
+			sent HTTP requests - ${obj.receivedResponses}`);
+		showJQGrid(obj);
+		showExportDiv(obj);
+
+		setTimeout(function () {
+			document.getElementById('tempUiElements').remove();
+		}, 3000);
+	}
+
+	function promiseGetFullInfo(obj, arr) {
+		return new Promise(function (resolve, reject) {
+			getFullInfo(obj, arr, resolve);
+		});
+	}
+
+	function prepareHtmlElementsUserDetails(obj, arr) {
+		updateStatusDiv(`Found users ${arr.length}`);
+		document.getElementById("intersection_title").textContent = "Getting the detailed info";
+		htmlElements.intersection.asProgress({
+			namespace: 'progress',
+			min: 0,
+			max: arr.length,
+			goal: arr.length,
+			labelCallback(n) {
+				const percentage = this.getPercentage(n);
+				return `Users: ${obj.processedUsers}/${arr.length}/${percentage}%`;
+			}
+		});
 	}
 
 	function showJQGrid(obj) {
@@ -82,7 +202,6 @@ $(function () {
 			data: myData,
 			rowNum: instaDefOptions.gridPageSize,
 			autowidth: true,
-			//shrinkToFit: true,
 			height: "100%",
 			rownumbers: true,
 			colModel: [{
@@ -98,7 +217,6 @@ $(function () {
 			}, {
 				label: 'Info',
 				name: 'id',
-				//width: '200',
 				sortable: false,
 				formatter: function (cellvalue, model, row) {
 					var ret = `id:${row.id}<br/>username:<strong>${row.username}</strong><br/>`;
@@ -116,7 +234,6 @@ $(function () {
 				name: 'biography',
 				sortable: false,
 				formatter: function (cellvalue, model, row) {
-					//	return cellvalue ? `<p>${cellvalue}</p>` : "";
 					return cellvalue ? cellvalue : "";
 				},
 				cellattr: function (rowId, tv, rawObject, cm, rdata) {
@@ -256,241 +373,11 @@ $(function () {
 				closeOnEscape: true,
 				searchOnEnter: true,
 				showQuery: true
-			}, // pSearch (works with these options)
-			{}).jqGrid('setGridWidth', $('#jqGrid').width() - 20); //TODO: find why autowidth doesn't work
+			}, 
+			{}).jqGrid('setGridWidth', $('#jqGrid').width() - 20); //TODO: why autowidth doesn't work? what is taken into account
 
 	}
 
-	function showExportDiv(obj) {
-
-		$("#exportDiv").show();
-
-		$("#export_XLSX").on("click", function () {
-			$("#jqGrid").jqGrid("exportToExcel", {
-				includeLabels: true,
-				includeGroupHeader: false,
-				includeFooter: false,
-				fileName: `user_${obj.userName}_${exportUtils.formatDate(new Date())}.xlsx`,
-				replaceStr: exportUtils.replaceStr
-			});
-		});
-	}
-
-	function prepareHtmlElements(obj) {
-
-		//statusDiv = document.getElementById('status');
-
-		if (obj.callBoth || ("followed_by" === obj.relType)) {
-			document.getElementById("followed_by_title").textContent = `${obj.userName} is followed by ${obj.followed_by_count} users`;
-			document.getElementById("followed_by_title").style.display = "block";
-			htmlElements.followed_by.show().asProgress({
-				namespace: 'progress',
-				min: 0,
-				max: obj.followed_by_count,
-				goal: obj.followed_by_count,
-				labelCallback(n) {
-					const percentage = this.getPercentage(n);
-					return `Followed by:${obj.followed_by_processed}/${obj.followed_by_count}/${percentage}%`;
-				}
-			});
-		}
-		if (obj.callBoth || ("follows" === obj.relType)) {
-			document.getElementById("follows_title").textContent = `${obj.userName} follows ${obj.follows_count} users`;
-			document.getElementById("follows_title").style.display = "block";
-			htmlElements.follows.show().asProgress({
-				namespace: 'progress',
-				min: 0,
-				max: obj.follows_count,
-				goal: obj.follows_count,
-				labelCallback(n) {
-					const percentage = this.getPercentage(n);
-					return `Follows:${obj.follows_processed}/${obj.follows_count}/${percentage}%`;
-				}
-			});
-		}
-	}
-
-	function updateProgressBar(obj, count) {
-		var newValue = 0 + obj[obj.relType + "_processed"] + count;
-		htmlElements[obj.relType].asProgress("go", newValue);
-		obj[obj.relType + "_processed"] = newValue;
-	}
-
-	function stopProgressBar(obj) {
-		htmlElements[obj.relType].asProgress("finish").asProgress("stop");
-	}
-
-	function generationCompleted(obj) {
-		clearInterval(obj.timerInterval);
-		var timer = document.querySelector('#timer');
-		htmlElements.intersection.asProgress("finish").asProgress("stop");
-
-		var diffFollowed = "", diffFollows = "";
-		if (obj.followed_by_count != obj.followed_by_processed) {
-			diffFollowed = `(actually returned ${obj.followed_by_processed})`;
-		}
-		if (obj.follows_count != obj.follows_processed) {
-			diffFollows = `(actually returned ${obj.follows_processed})`;
-		}
-
-		updateStatusDiv(`Completed, spent time - ${timer.textContent}, 
-			created list length - ${myData.length} (follows - ${obj.follows_count}${diffFollows}, 
-			followed by - ${obj.followed_by_count}${diffFollowed}),
-			sent HTTP requests - ${obj.receivedResponses}`);
-		showJQGrid(obj);
-		showExportDiv(obj);
-
-		setTimeout(function () {
-			document.getElementById('tempUiElements').remove();
-		}, 3000);
-	}
-
-
-	function fetchInstaUsers(obj) {
-		var urlTemplate = `https://www.instagram.com/graphql/query/?query_id=${instaDefOptions.queryId[obj.relType]}&id=${obj.userId}&first=${obj.pageSize}`;
-		obj.url = obj.url || urlTemplate;
-		$.ajax({
-			url: obj.url,
-			method: 'GET',
-			headers: {
-				"X-CSRFToken": obj.csrfToken,
-				"eferer": "https://www.instagram.com/" + obj.userName + "/"
-			},
-			success: function (res, textStatus, xhr) {
-				obj.receivedResponses += 1;
-				var data = res.data.user[Object.keys(res.data.user)[0]];
-				updateStatusDiv(`received users - ${data.edges.length} (${obj.relType}/${obj.receivedResponses})`);
-				for (let i = 0; i < data.edges.length; i++) {
-					var found = false;
-					if (obj.checkDuplicates) { //only when the second run happens (or we started with already opened result page)
-						for (let j = 0; j < myData.length; j++) {
-							if (data.edges[i].node.username === myData[j].username) {
-								found = true;
-								myData[j]["user_" + obj.relType] = true;
-								break;
-							}
-						}
-					}
-					if (!(found)) {
-						data.edges[i].node.user_follows = false; //explicitly set the value for correct search
-						data.edges[i].node.user_followed_by = false; //explicitly set the value for correct search
-						data.edges[i].node["user_" + obj.relType] = true;
-						myData.push(data.edges[i].node);
-					}
-				}
-				updateProgressBar(obj, data.edges.length);
-
-				if (data.page_info.has_next_page) { //need to continue
-					obj.url = `${urlTemplate}&after=${data.page_info.end_cursor}`;
-					setTimeout(function () {
-						fetchInstaUsers(obj);
-					}, calculateTimeOut(obj));
-				} else {
-					stopProgressBar(obj);
-					if (obj.callBoth) {
-						obj.url = null;
-						obj.relType = obj.relType === "follows" ? "followed_by" : "follows";
-						obj.callBoth = false;
-						obj.checkDuplicates = true;
-						setTimeout(function () {
-							fetchInstaUsers(obj);
-						}, calculateTimeOut(obj));
-					} else {
-						//we are done
-						prepareHtmlElementsForIntersection(obj, myData);
-						promiseGetFullInfo(obj, myData).then(function () {
-							generationCompleted(obj);
-						});
-					}
-				}
-			},
-			error: function (jqXHR, exception) {
-				console.log("error ajax");
-				console.log(arguments);
-				if (jqXHR.status === 0) {
-					setTimeout(function () {
-						fetchInstaUsers(obj);
-					}, instaDefOptions.retryInterval); //TODO: Test and make configurable
-					alert(messages.getMessage("NOTCONNECTED", +instaDefOptions.retryInterval / 60000));
-				} else if (jqXHR.status === 429) {
-					console.log("HTTP429 error.", new Date());
-					retryError(jqXHR.status, obj);
-				} else if (jqXHR.status == 404) {
-					alert(messages.getMessage("HTTP404"));
-				} else if (jqXHR.status == 500) {
-					alert(messages.getMessage("HTTP500"));
-				} else if (exception === 'parsererror') {
-					alert(messages.getMessage("JSONPARSEERROR"));
-				} else if (exception === 'timeout') {
-					alert(messages.getMessage("TIMEOUT"));
-				} else if (exception === 'abort') {
-					alert(messages.getMessage("AJAXABORT"));
-				} else {
-					alert(messages.getMessage("UNCAUGHT", jqXHR.responseText));
-				}
-			}
-		});
-	}
-
-	function retryError(code, obj) {
-		console.log("HTTP error", new Date());
-		updateStatusDiv(messages.getMessage("HTTP429", +instaDefOptions.retryInterval / 60000), "red");
-		timeout.setTimeout(3000)
-			.then(function () {
-				return countdown.doCountdown("status", "", (new Date()).getTime() + +instaDefOptions.retryInterval)
-			})
-			.then(function () {
-				console.log("Continue execution after HTTP error", new Date());
-				fetchInstaUsers(obj);
-			});
-
-	}
-
-	function promiseGetFullInfo(obj, arr) {
-		return new Promise(function (resolve, reject) {
-			obj.processedUsers = 0;
-			getFullInfo(obj, arr, resolve);
-		});
-	}
-
-	function prepareHtmlElementsForIntersection(obj, arr) {
-
-		updateStatusDiv(`Found users ${arr.length}`);
-		document.getElementById("intersection_title").textContent = "Getting the detailed info";
-		htmlElements.intersection.asProgress({
-			namespace: 'progress',
-			min: 0,
-			max: arr.length,
-			goal: arr.length,
-			labelCallback(n) {
-				const percentage = this.getPercentage(n);
-				return `Users: ${obj.processedUsers}/${arr.length}/${percentage}%`;
-			}
-		});
-	}
-
-
-	function getFullInfo(obj, arr, resolve) {
-		userInfo.getUserProfile(arr[obj.processedUsers].username).then(function (user) {
-			myData[obj.processedUsers] = $.extend({}, myData[obj.processedUsers], user);;
-			obj.receivedResponses++;
-			htmlElements.intersection.asProgress("go", obj.processedUsers++);
-			if (obj.processedUsers === arr.length) {
-				resolve();
-				return;
-			}
-			setTimeout(function () {
-				getFullInfo(obj, arr, resolve);
-			}, 0);
-		});
-	}
-
-	function calculateTimeOut(obj) {
-		if (instaDefOptions.noDelayForInit && (obj.receivedResponses < instaDefOptions.requestsToSkipDelay)) {
-			return 0;
-		}
-		return obj.delay;
-	}
 
 });
 
